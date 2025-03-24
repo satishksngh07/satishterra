330273f18beab4fa3f5184ed6c8a920c29b9a1d5
satish <satishksngh@gmail.com>
2025-03-24 15:35:52 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example2" {
+  name     = "example-satish"
+  location = "West Europe"
+}
